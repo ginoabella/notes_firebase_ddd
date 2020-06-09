6232f212d14cd1a563c55e14a99841e580cb03c7
gino abella <gino@ginos-MacBook-Air.local>
2020-06-09 12:34:43 +0800

    part 12

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,6 +1,46 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/collection.dart';
 
 import 'package:notes_firebase/domain/core/failures.dart';
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLenght,
+) {
+  if (input.length <= maxLenght) {
+    return right(input);
+  } else {
+    return left(
+        ValueFailure.exceedingLength(fieldValue: input, max: maxLenght));
+  }
+}
+
+Either<ValueFailure<String>, String> validdateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(fieldValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (!input.contains('\n')) {
+    return right(input);
+  } else {
+    return left(ValueFailure.multiLine(fieldValue: input));
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLenght<T>(
+  KtList<T> input,
+  int maxLenght,
+) {
+  if (input.size <= maxLenght) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(fieldValue: input, max: maxLenght));
+  }
+}
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   // Maybe not the most robust way of email validation but it's good enough
